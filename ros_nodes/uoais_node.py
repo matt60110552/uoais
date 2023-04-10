ef8d8022705bc9d7f3770043affc4ab9a4a3a430
matt60110552 <matt6011055.eed07@nctu.edu.tw>
2023-04-10 17:55:34 +0800

    after 4/10 meeting

--- ros_nodes/uoais_node.py
+++ ros_nodes/uoais_node.py
@@ -21,6 +21,8 @@ from adet.utils.post_process import detector_postprocess, DefaultPredictor
 from foreground_segmentation.model import Context_Guided_Network
 
 from sensor_msgs.msg import PointCloud2, PointField
+from sensor_msgs.point_cloud2 import create_cloud_xyz32
+from grasp_msg.srv import GraspSegment, GraspSegmentResponse
 from geometry_msgs.msg import TransformStamped
 from std_msgs.msg import String, Header
 from sensor_msgs.msg import Image, CameraInfo, RegionOfInterest
@@ -38,6 +40,7 @@ class UOAIS():
         rospy.loginfo("Starting uoais node with {} mode".format(self.mode))
         self.rgb_topic = rospy.get_param("~rgb", "/camera/color/image_raw")
         self.depth_topic = rospy.get_param("~depth", "/camera/aligned_depth_to_color/image_raw")
+        # self.depth_topic = rospy.get_param("~depth", "/camera/depth/image_rect_raw")
         camera_info_topic = rospy.get_param("~camera_info", "/camera/color/camera_info")
         # UOAIS-Net
         self.det2_config_file = rospy.get_param("~config_file", 
@@ -54,7 +57,8 @@ class UOAIS():
         self.ransac_iter = rospy.get_param("~ransac_iter", 10)
 
         self.cv_bridge = cv_bridge.CvBridge()
-        
+        self.count = 0
+        self.vis = False
         # initialize UOAIS-Net and CG-Net
         self.load_models()
         """
@@ -89,7 +93,8 @@ class UOAIS():
             rospy.loginfo("uoais results at topic: /uoais/results")
 
         elif self.mode == "service":
-            self.srv = rospy.Service('/get_uoais_results', UOAISRequest, self.service_callback)
+            # self.srv = rospy.Service('/get_uoais_results', UOAISRequest, self.service_callback)
+            self.srv = rospy.Service('/get_uoais_results', GraspSegment, self.service_callback)
             self.points_pub = rospy.Publisher("/uoais/Pointclouds", PointCloud2, queue_size=10)
             rospy.loginfo("uoais results at service: /get_uoais_results")
         else:
@@ -97,9 +102,7 @@ class UOAIS():
         self.vis_pub = rospy.Publisher("/uoais/vis_img", Image, queue_size=10)
         rospy.loginfo("visualization results at topic: /uoais/vis_img")
 
-
     def load_models(self):
-
         # UOAIS-Net
         self.det2_config_file = os.path.join(Path(__file__).parent.parent, self.det2_config_file)
         rospy.loginfo("Loading UOAIS-Net with config_file: {}".format(self.det2_config_file))
@@ -120,7 +123,6 @@ class UOAIS():
             self.fg_model.cuda()
             self.fg_model.eval()
 
-
     def topic_callback(self, rgb_msg, depth_msg):
 
         results = self.inference(rgb_msg, depth_msg)        
@@ -130,9 +132,9 @@ class UOAIS():
 
         rgb_msg = rospy.wait_for_message(self.rgb_topic, Image)
         depth_msg = rospy.wait_for_message(self.depth_topic, Image)
-        results = self.inference(rgb_msg, depth_msg)        
-        return UOAISRequestResponse(results)
-        
+        results = self.inference(rgb_msg, depth_msg)
+        # return UOAISRequestResponse(results)
+        return GraspSegmentResponse(results)
 
     def inference(self, rgb_msg, depth_msg):
         start_time = time.time()
@@ -143,6 +145,7 @@ class UOAIS():
         depth_img = normalize_depth(depth)
         depth_img = cv2.resize(depth_img, (self.W, self.H), interpolation=cv2.INTER_NEAREST)
         depth_img = inpaint_depth(depth_img)
+        self.count += 1
         print("0 time: {}".format(time.time() - start_time))
         # UOAIS-Net inference
         if self.cfg.INPUT.DEPTH and self.cfg.INPUT.DEPTH_ONLY:
@@ -207,12 +210,12 @@ class UOAIS():
         idx_shuf = np.concatenate((np.where(pred_occs==1)[0] , np.where(pred_occs==0)[0] )) 
         preds, pred_visibles, pred_occs, pred_bboxes = \
             preds[idx_shuf], pred_visibles[idx_shuf], pred_occs[idx_shuf], pred_bboxes[idx_shuf]
-        # vis_img = visualize_pred_amoda_occ(rgb_img, preds, pred_bboxes, pred_occs)
-        # if self.use_cgnet or self.use_planeseg:
-        #     vis_fg = np.zeros_like(rgb_img) 
-        #     vis_fg[:, :, 1] = fg_output * 255
-        #     vis_img = cv2.addWeighted(vis_img, 0.8, vis_fg, 0.2, 0)
-        # self.vis_pub.publish(self.cv_bridge.cv2_to_imgmsg(vis_img, encoding="bgr8"))
+        vis_img = visualize_pred_amoda_occ(rgb_img, preds, pred_bboxes, pred_occs)
+        if self.use_cgnet or self.use_planeseg:
+            vis_fg = np.zeros_like(rgb_img) 
+            vis_fg[:, :, 1] = fg_output * 255
+            vis_img = cv2.addWeighted(vis_img, 0.8, vis_fg, 0.2, 0)
+        self.vis_pub.publish(self.cv_bridge.cv2_to_imgmsg(vis_img, encoding="bgr8"))
         # pubish the uoais results
         results = UOAISResults()
         # results.header = rgb_msg.header
@@ -243,7 +246,7 @@ class UOAIS():
             """
             convert camera_color_optical_frame to camer_link
             """
-            transform_stamped = self.tf_buffer.lookup_transform('base_link', depth_msg.header.frame_id, rospy.Time(0))
+            transform_stamped = self.tf_buffer.lookup_transform('base', depth_msg.header.frame_id, rospy.Time(0))
             trans = np.array([transform_stamped.transform.translation.x,
                               transform_stamped.transform.translation.y,
                               transform_stamped.transform.translation.z])
@@ -258,59 +261,68 @@ class UOAIS():
             rospy.logerr("Failed to transform point cloud: {}".format(e))
             return
 
-
-
         print("3 time: {}".format(time.time() - start_time))
-
-
+        print("T: {}".format(T))
         if self.target_pointcloud is None:
             np.set_printoptions(threshold=np.inf)
             ave_depth_list = []
             depth_list = []
-            in_for_time = time.time()
+            mask_center_list = []
             for i in range(len(pred_visibles)):
-                mask = np.array(pred_visibles[i]).astype(int)
+                mask = np.array(pred_visibles[i]).astype(np.uint8)
+                kernel = np.ones((3, 3), np.uint8)
+                mask = cv2.erode(mask, kernel, iterations=1)
+
+                # choose by center
+                indices = np.where(mask == 1)
+                center_index = np.round(np.mean(indices, axis=1)).astype(int)
+                print("center_index: {}".format(center_index))
+                # mask_center_list.append(np.linalg.norm(center_index - np.array([240, 320])))
                 # cv2.imshow('Image', mask.astype(np.uint8)*255)
                 # # Wait for a key press to exit
                 # cv2.waitKey(0)
                 # # Close all windows
                 # cv2.destroyAllWindows()
 
-
                 un_depth_img = unnormalize_depth(depth_img)
                 un_depth_img[np.logical_not(mask)] = 0
-                kernel = np.ones((3, 3), np.uint8)
-                un_depth_img = cv2.erode(un_depth_img, kernel, iterations=1)
+                # choose by depth
                 ave_depth_list.append(np.mean(un_depth_img[un_depth_img != 0]))
+
                 depth_list.append(un_depth_img)
+            # choose by depth
             un_depth_img = depth_list[np.argmin(ave_depth_list)]
+            
+            # choose by center
+            # un_depth_img = depth_list[np.argmin(mask_center_list)]
 
             print("4 time: {}".format(time.time() - start_time))
         else:
-            in_for_time = time.time()
             """
             project pointcloud back to plane
             """
             previews_pointcloud = o3d.geometry.PointCloud()
-            previews_pointcloud.points = o3d.utility.Vector3dVector(self.target_pointcloud.points)
+            previews_points = np.asarray(self.target_pointcloud.points).copy()
+            previews_pointcloud.points = o3d.utility.Vector3dVector(previews_points)
             previews_pointcloud.transform(T_inv)
-            target_pointcloud_cam = np.asarray(previews_pointcloud.points).copy()
+            target_points_cam = np.asarray(previews_pointcloud.points)
             fx = self.o3d_camera_intrinsic.intrinsic_matrix[0, 0]
             fy = self.o3d_camera_intrinsic.intrinsic_matrix[1, 1]
             cx = self.o3d_camera_intrinsic.intrinsic_matrix[0, 2]
             cy = self.o3d_camera_intrinsic.intrinsic_matrix[1, 2]
             projected_target_pixel = []
-            for i in range(len(target_pointcloud_cam)):
-                u = target_pointcloud_cam[i, 0] * fx / target_pointcloud_cam[i, 2] + cx
-                v = target_pointcloud_cam[i, 1] * fy / target_pointcloud_cam[i, 2] + cy
-                projected_target_pixel.append([u, v])
+            for i in range(len(target_points_cam)):
+                u = target_points_cam[i, 0] * fx / target_points_cam[i, 2] + cx
+                v = target_points_cam[i, 1] * fy / target_points_cam[i, 2] + cy
+                if 0 <= u < 480 and 0 <= v < 640:
+                    projected_target_pixel.append([u, v])
             projected_target_pixel = np.asarray(projected_target_pixel)
             projected_target_pixel = projected_target_pixel.astype(int)
-            projected_target_pixel = np.asarray(projected_target_pixel[:, :2])
+            # projected_target_pixel = np.asarray(projected_target_pixel[:, :2])
             pixel_set, _ = np.unique(projected_target_pixel, axis=0, return_index=True)
             # print("pixel_set: {}".format(pixel_set))
             target_area = np.zeros((480, 640), dtype=int)
-            target_area[pixel_set[:, 1] - 1, pixel_set[:, 0] - 1] = 1  # u and v start with 1
+            target_area[pixel_set[:, 1], pixel_set[:, 0]] = 1  # u and v start with 1
 
             # # # print("target_area: {}".format(target_area))
             # cv2.imshow('Image', target_area.astype(np.uint8)*255)
@@ -328,14 +340,16 @@ class UOAIS():
             for i in range(len(pred_visibles)):
                 mask = np.array(pred_visibles[i]).astype(int)
                 overlap = np.logical_and(mask, target_area)
-                overlap_list.append(np.count_nonzero(overlap))
+                overlap_list.append(np.count_nonzero(overlap)/np.count_nonzero(mask))
 
-            final_mask = np.array(pred_visibles[np.argmax(overlap_list)]).astype(int)
+            kernel = np.ones((3, 3), np.uint8)
+            final_mask = np.array(pred_visibles[np.argmax(overlap_list)]).astype(np.uint8)
+            final_mask = cv2.erode(final_mask, kernel, iterations=1)
             # final_mask = np.array(pred_visibles[0]).astype(int)  # randomly choose, remember to delete
             un_depth_img = unnormalize_depth(depth_img)
             un_depth_img[np.logical_not(final_mask)] = 0
-            kernel = np.ones((3, 3), np.uint8)
-            un_depth_img = cv2.erode(un_depth_img, kernel, iterations=1)
+            # kernel = np.ones((3, 3), np.uint8)
+            # un_depth_img = cv2.erode(un_depth_img, kernel, iterations=1)
             print("4 time: {}".format(time.time() - start_time))
 
         o3d_rgb_img = o3d.geometry.Image(rgb_img)
@@ -347,21 +361,48 @@ class UOAIS():
         sampled_pc, _ = o3d_pc.remove_statistical_outlier(nb_neighbors=20, std_ratio=1.0)
         if self.target_pointcloud is None:
             sampled_pc.transform(T)
-            # o3d.visualization.draw_geometries([sampled_pc])
-            pass
+
+            # visualize part
+            if self.vis:
+                o3d.visualization.draw_geometries([sampled_pc])
+            self.target_pointcloud = o3d.geometry.PointCloud()
+            self.target_pointcloud.points = o3d.utility.Vector3dVector(sampled_pc.points)
         else:
             sampled_pc.transform(T)
-            pass
+            # visualize part
             self.target_pointcloud.paint_uniform_color([1, 0, 0])
             sampled_pc.paint_uniform_color([0, 1, 0])
-            o3d.visualization.draw_geometries([self.target_pointcloud, sampled_pc])
+            if self.vis:
+                o3d.visualization.draw_geometries([self.target_pointcloud, sampled_pc])
 
-        self.target_pointcloud = o3d.geometry.PointCloud()
-        self.target_pointcloud.points = o3d.utility.Vector3dVector(sampled_pc.points)
+            # sampled_pc = sampled_pc.uniform_down_sample(2+self.count)
+            print(len(sampled_pc.points))
+            downsample_index = np.random.choice(int(len(sampled_pc.points)), int(int(len(sampled_pc.points)) * (0.95 ** self.count)), replace=True)
+            sampled_pc.points = o3d.utility.Vector3dVector(np.asarray(sampled_pc.points)[downsample_index, :])
+            print(len(sampled_pc.points))
+            merged_pcd = self.target_pointcloud + sampled_pc
+            # merged_pcd_downsampled = merged_pcd.voxel_down_sample(voxel_size=0.005)
+            # o3d.visualization.draw_geometries([merged_pcd_downsampled])
+            # self.target_pointcloud = o3d.geometry.PointCloud()
+            # self.target_pointcloud.points = o3d.utility.Vector3dVector(merged_pcd_downsampled.points)
+
+            # set the merged_pcd's points to a fixed number, 40000 is recommanded
+            fixed_size_index = np.random.choice(int(len(merged_pcd.points)), 4096, replace=True)
+            merged_pcd.points = o3d.utility.Vector3dVector(np.asarray(merged_pcd.points)[fixed_size_index, :])
+
+            if self.vis:
+                o3d.visualization.draw_geometries([merged_pcd])
+            self.target_pointcloud = o3d.geometry.PointCloud()
+            self.target_pointcloud.points = o3d.utility.Vector3dVector(merged_pcd.points)
+
+        # self.target_pointcloud = o3d.geometry.PointCloud()
+        # self.target_pointcloud.points = o3d.utility.Vector3dVector(sampled_pc.points)
 
         # pc = PointCloud2()
-        # pc.header.stamp = rospy.Time().now()
-        # pc.header.frame_id = "project_frame"
+        header = Header()
+        header.stamp = rospy.Time().now()
+        header.frame_id = "base"
+        pc = create_cloud_xyz32(header, np.asarray(self.target_pointcloud.points))
         # pc.fields = [
         #     PointField('x', 0, PointField.FLOAT32, 1),
         #     PointField('y', 4, PointField.FLOAT32, 1),
@@ -370,11 +411,12 @@ class UOAIS():
         # pc.width = len(np.asarray(self.target_pointcloud.points))
         # pc.is_bigendian = False
         # pc.point_step = 12
-        # pc.row_step = pc.point_step * len(np.asarray(self.target_pointcloud.points)) 
+        # pc.row_step = pc.point_step * len(np.asarray(self.target_pointcloud.points))
         # pc.is_dense = False
         # pc.data = np.asarray(self.target_pointcloud.points, np.float32).tostring()
-        # while True:
-        #     self.points_pub.publish(pc)
+        # for i in range(10):
+        self.points_pub.publish(pc)
+        #     time.sleep(1)
 
         """
         end of test
@@ -386,10 +428,11 @@ class UOAIS():
         """
         End of add by Matt
         """
-        return results
+        # return results
+        return pc
 
 
 if __name__ == '__main__':
 
     uoais = UOAIS()
-    rospy.spin() 
+    rospy.spin()
